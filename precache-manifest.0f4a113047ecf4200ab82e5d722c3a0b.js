887f8485ab2f09782e0e1aef37c3ebaf393a8e8b
Vipansh Thakur <vipansh@github.com>
2020-10-14 18:42:07 +0530

    Updates

---
+++ precache-manifest.0f4a113047ecf4200ab82e5d722c3a0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e23297810df4db0ff8061dc9622cbde",
+    "url": "/ecommerce1.0/index.html"
+  },
+  {
+    "revision": "f770b263425f1d7f6a06",
+    "url": "/ecommerce1.0/static/css/main.b2d9416d.chunk.css"
+  },
+  {
+    "revision": "d382318c8aba0f08029c",
+    "url": "/ecommerce1.0/static/js/2.fa32d9dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce1.0/static/js/2.fa32d9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f770b263425f1d7f6a06",
+    "url": "/ecommerce1.0/static/js/main.d9f7712b.chunk.js"
+  },
+  {
+    "revision": "3f04f821c9e687700ee5",
+    "url": "/ecommerce1.0/static/js/runtime-main.64b55bc9.js"
+  },
+  {
+    "revision": "9354ed0aa26fd6180864affaa656217d",
+    "url": "/ecommerce1.0/static/media/accelerator.9354ed0a.jpg"
+  },
+  {
+    "revision": "4f2bfca937406e2f65e3c7abac25c39b",
+    "url": "/ecommerce1.0/static/media/airport.4f2bfca9.jpg"
+  },
+  {
+    "revision": "6d80b19c5e9ad39fe3b13e4bd7d61523",
+    "url": "/ecommerce1.0/static/media/alchemy.6d80b19c.jpg"
+  },
+  {
+    "revision": "229537af17c27ecba9f86b06a4b1df48",
+    "url": "/ecommerce1.0/static/media/laptop.229537af.jpg"
+  },
+  {
+    "revision": "6b5f7c097efe72dd11ebb4658060bb90",
+    "url": "/ecommerce1.0/static/media/vipansh-pic.6b5f7c09.jpg"
+  }
+]);
